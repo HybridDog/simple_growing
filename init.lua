c545a269369b24d6f64db69e494aedfc64992e96
HybridDog <adresse_test@t-online.de>
2013-02-01 19:56:47 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+--------------------------------------Simple Growing 12.12----------------------------------------
+
+PAPYRUS_GROUND = {"default:papyrus", "default:dirt_with_grass", "default:dirt", "default:sand"}
+ CACTUS_GROUND = {"default:cactus", "default:sand", "default:desert_sand"}
+
+simple_growing("default:papyrus", PAPYRUS_GROUND, 20, 6, 5)
+simple_growing("default:cactus", CACTUS_GROUND, 20, 12, 4)
